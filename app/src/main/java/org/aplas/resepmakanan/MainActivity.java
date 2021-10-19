7a3141956c73686f0ee105c7f05311c9bb1d396b
MuhammadRezaPahlevi0218 <reyzae99@gmail.com>
2021-10-19 07:15:54 +0700

    membuat on click dan detai makanan

--- app/src/main/java/org/aplas/resepmakanan/MainActivity.java
+++ app/src/main/java/org/aplas/resepmakanan/MainActivity.java
@@ -11,6 +11,7 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
     private ArrayList<String> foto = new ArrayList<>();
     private ArrayList<String> nama = new ArrayList<>();
+    private ArrayList<String> info = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
     }
     private void proses(){
         RecyclerView recyclerView = findViewById(R.id.recyleview);
-        RecycleViewAdapter adapter = new RecycleViewAdapter(foto, nama, this);
+        RecycleViewAdapter adapter = new RecycleViewAdapter(foto, nama, info, this);
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
@@ -29,9 +30,11 @@ public class MainActivity extends AppCompatActivity {
     private void getData(){
         nama.add("Soto");
         foto.add("https://cdns.klimg.com/merdeka.com/i/w/news/2017/11/06/906065/670x335/4-resep-dan-cara-membuat-soto-ayam-bening-enak-dan-sederhana-kln.jpg");
+        info.add("aswadsawdsawdasdwasdawdssawsdawdsawdsawdsawdsawdsa");
 
         nama.add("Pecel");
         foto.add("https://th.bing.com/th/id/OIP.4YbvSMtH4L63HZGxOVQIYgHaEK?w=263&h=180&c=7&r=0&o=5&pid=1.7");
+        info.add("aswadsawdsawdasdwasdawdssawsdawdsawdsawdsawdsawdsa");
 
         proses();
     }
